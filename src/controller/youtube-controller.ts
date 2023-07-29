c83bae7caa49950292486f352242846db619caa7
Marcoant007 <marcoantnovo@gmail.com>
2023-07-29 20:40:50 -0300

    finally upload for youtube

--- src/controller/youtube-controller.ts
+++ src/controller/youtube-controller.ts
@@ -1,11 +1,12 @@
 import { Request, Response, Router } from "express";
 import multer from "multer";
 import path from "path";
-import { getTokenUserAuthorization, uploadVideoService } from "../services/youtube-service";
+import { getTokenUserAuthorization, googleGlobalOauhAuthenication, uploadVideoService, youtubeSendVideo } from "../services/youtube-service";
 
 const youtubeRoutes = Router();
 youtubeRoutes.get("/", async(request: Request, response: Response) => {
-    return response.render('index');
+    const oauthUrl = await uploadVideoService();
+    return response.render('oauth', { oauthUrl: oauthUrl });
 });
 
 const storage = multer.diskStorage({
@@ -24,10 +25,8 @@ youtubeRoutes.post('/upload', upload.single('file'), async (request: Request, re
         if(!request.file) {
             response.send('Arquivo não enviado');
         } else {
-           const title = request.file.filename; // update this
-           const description = 'Ta subindo o videozin'; // update this
-           const oauthUrl = await uploadVideoService(request.file.path, title, description);
-           response.render('oauth', { oauthUrl: oauthUrl });
+           const youtubeVideo = await youtubeSendVideo(request.file);
+           response.send(youtubeVideo);
         }
     } catch (error) {
         console.log(error);
@@ -37,10 +36,10 @@ youtubeRoutes.post('/upload', upload.single('file'), async (request: Request, re
 youtubeRoutes.get('/oauth/callback', async (request: Request, response: Response) => {
     try {
       const authCode = request.query.code as string;
-      const tokens = await getTokenUserAuthorization(authCode);
-      const acessToken = tokens.access_token;
-      console.log(tokens)
-      return response.render('youtubeSend', {acessToken: acessToken});
+      const oauth = await getTokenUserAuthorization(authCode);
+      const acessToken = oauth.tokens.access_token;
+      await googleGlobalOauhAuthenication(oauth.oauth2Client, oauth.tokens)
+      return response.render('upload', {acessToken: acessToken});
     } catch (error) {
       console.log(error);
       return response.status(500).send('Error during authentication');
--- src/services/youtube-service.ts
+++ src/services/youtube-service.ts
@@ -6,10 +6,11 @@ import { oauth2 } from "googleapis/build/src/apis/oauth2";
 const OAuth2 = google.auth.OAuth2;
 import axios, { AxiosResponse } from "axios";
 import { exec } from 'child_process';
+import { Credentials } from "google-auth-library";
+const youtube = google.youtube({ version: 'v3' });
+import fs from "fs"
 
-
-
-export async function uploadVideoService(filePath: any, title: string, description: string) {
+export async function uploadVideoService() {
   const oauth2 = await createOAuthClient();
   const consentUrl = oauth2.generateAuthUrl({
     access_type: 'offline',
@@ -17,7 +18,7 @@ export async function uploadVideoService(filePath: any, title: string, descripti
     response_type: 'code'
   });
   return consentUrl;
-}
+};
 
 async function createOAuthClient() {
   const OAuthCliente = new OAuth2(
@@ -25,13 +26,87 @@ async function createOAuthClient() {
     credentials.web.client_secret,
     credentials.web.redirect_uris[0]
   );
-
   return OAuthCliente;
-}
+};
 
 export async function getTokenUserAuthorization(authorizationCode: string) {
   const oauth2Client = await createOAuthClient();
-  const {tokens} = await oauth2Client.getToken(authorizationCode);
+  const { tokens } = await oauth2Client.getToken(authorizationCode);
   oauth2Client.setCredentials(tokens);
-  return tokens;
+  const oauth = {
+    oauth2Client,
+    tokens
+  }
+  return oauth;
+};
+
+export async function googleGlobalOauhAuthenication(oauth2Client: any, tokens: Credentials) {
+  google.options({
+    auth: oauth2Client
+  });
 }
+
+export async function youtubeSendVideo(file: any){
+  const videoFilePath = file.path;
+  const videoFileSize = file.size;
+  const videoTitle = file.originalname;
+  const videoTags = ['default']; // tags devem ser um array de strings
+  const videoDescription = "upload video from youtube api";
+
+  const requestParameters = {
+    part: ['snippet', 'status'],
+    requestBody: {
+      snippet: {
+        title: videoTitle,
+        description: videoDescription,
+        tags: videoTags
+      },
+      status: {
+        privacyStatus: 'unlisted'
+      }
+    },
+    media: {
+      body: fs.createReadStream(videoFilePath)
+    }
+  }
+  const youtubeResponse = await youtube.videos.insert(requestParameters, { // Adicionado await aqui
+  });
+
+  const message = `Video available at https://youtu.be/${youtubeResponse.data.id}`
+  
+  // const videoId = youtubeResponse.data.id;
+  // const videoThumbnalFilePath = '../img/images.jpg';
+
+  // if (videoId) { // Adicionada verificação aqui
+  //   const requestParametersThumb = {
+  //     videoId: videoId,
+  //     media: {
+  //       mimeType: 'image.jpeg',
+  //       body: fs.createReadStream(videoThumbnalFilePath)
+  //     }
+  //   }
+
+  //   const youtubeResponseThumb = await youtube.thumbnails.set(requestParametersThumb)
+  //   console.log('Thumb uploaded');
+
+    
+  // } else {
+  //   console.log('Video ID is null. Thumbnail was not set.');
+  // }
+
+  return message;
+}
+
+
+// {
+//   "fieldname": "file",
+//   "originalname": "Valorant 2023.06.30 - 01.07.04.02.DVR.mp4",
+//   "encoding": "7bit",
+//   "mimetype": "video/mp4",
+//   "destination": "src/upload/",
+//   "filename": "1690670261057.mp4",
+//   "path": "src\\upload\\1690670261057.mp4",
+//   "size": 493639442
+// }
+
+
